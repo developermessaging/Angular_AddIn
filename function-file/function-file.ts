3740f4e5ab5bf0397b952841ad665b9fb07c778b
rusilva <rusilva@RUSILVA-SPRO-01>
2020-03-05 15:36:39 +0000

    Changes to Manifest and Flow Form

--- function-file/function-file.ts
+++ function-file/function-file.ts
@@ -6,10 +6,46 @@
 (() => {
   // The initialize function must be run each time a new page is loaded
   Office.initialize = () => {
-    var i:string = "1";
-
-    i = "test";
+    console.log("Initialize function called insertDefaultCard");
+    //console.log(Office.context.roamingSettings.get("RoamingSettingsTest"));
   };
 
   // Add any ui-less function here
+  // Helper function to add a status message to the info bar.
+
+
+ async function insertDefaultCard(event) {
+  //console.log("insertDefaultCard -try");
+  Office.context.mailbox.item.notificationMessages.addAsync("error", {
+         type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+         message: "RoamingSettingTest : "
+       });
+  event.completed();
+
+  // try {
+  //   console.log("insertDefaultCard -try");
+  //   var roamingsetting = Office.context.roamingSettings.get("RoamingSettingTest");
+  //   Office.context.mailbox.item.notificationMessages.addAsync("error", {
+  //     type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+  //     message: "RoamingSettingTest : " + roamingsetting,
+  //   });
+
+  // } catch (error) {
+  //   Office.context.mailbox.item.notificationMessages.addAsync("error", {
+  //     type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+  //     message: "RoamingSettingTest Error : " + JSON.stringify(error)
+  //   });
+    
+  // }
+  // finally{
+  //   event.completed();
+  // }  
+
+  
+
+}
+
+
+
+
 })();
